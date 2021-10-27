5ea50c29b76950e9c2426c7dc98f3c3481975e8b
MixPiyadanai <mix.piyadanai@gmail.com>
2021-10-27 14:26:13 +0700

    111

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document{
   <title>AdminLTE 3 | Dashboard 3</title>
   {/* Google Font: Source Sans Pro */}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
   <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
   {/* Font Awesome Icons */}
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css" />
